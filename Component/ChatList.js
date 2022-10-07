02cdba00b1d2218322bde650d1efa1aa12aec601
johan <jroq232@icloud.com>
2022-10-06 20:29:20 -0400

    display most recent message in chat list

--- Component/ChatList.js
+++ Component/ChatList.js
@@ -2,55 +2,82 @@ import {StyleSheet, Text, View, SafeAreaView, FlatList} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import DATA from './Data';
 import Messages from './Messages';
+import firestore, {firebase} from '@react-native-firebase/firestore';
 
 import {db} from './firebase-config';
 import {
   collection,
-  addDoc,
   doc,
   query,
   limit,
   onSnapshot,
-  serverTimestamp,
   orderBy,
-  getDoc,
+  updateDoc,
 } from 'firebase/firestore';
 const ChatList = () => {
   const [chatList, setChatList] = useState('');
 
-  //Get all messages in firebase collection
-  const querySnapshot = async () => {
+  //Get all chats in firebase collection
+  const queryAllChats = async () => {
+    //Create refrence for chat collection
     const chatRef = collection(db, 'users', 'Guest@buzz.com', 'chats');
-    const lastMessageRef = collection(
-      db,
-      'users',
-      'Guest@buzz.com',
-      'chats',
-      'Buzz',
-      'messages',
-    );
+    //Query (get all documents from a collection) 50 messages from chat collection.
+    const chatQuery = query(chatRef, limit(50));
 
-    const q = query(chatRef, limit(50));
-    const lastMessage = query(lastMessageRef, orderBy('timestamp'), limit(1));
-    const unsubscribe = onSnapshot(q, querySnapshot => {
+    const unsub = onSnapshot(chatQuery, querySnapshot => {
+      //Create empty array to hold our chat objects..
       const chats = [];
+      var placeholderLastMessage = '';
 
+      //Enumerate through each doc.
       querySnapshot.forEach(doc => {
+        //Find last message of each chat
+
+        //Create refrence for collection of messages with current user
+        const messagesRef = collection(
+          db,
+          'users',
+          'Guest@buzz.com',
+          'chats',
+          doc.data()['user'],
+          'messages',
+        );
+        //Query messages of current user, get most recent message
+        const lastMessageQuery = query(
+          messagesRef,
+          limit(1),
+          orderBy('timestamp', 'desc'),
+        );
+
+        onSnapshot(lastMessageQuery, querySnapshot => {
+          querySnapshot.forEach(doc => {
+            placeholderLastMessage = doc.data()['text'];
+            //Find this chat then set lastMessage field as most recent message
+          });
+          console.log(doc.data()['user']);
+          console.log(placeholderLastMessage);
+          setLastMessageField(doc.data()['user'], placeholderLastMessage);
+        });
         chats.push({
           ...doc.data(),
           id: doc.id,
           user: doc.data()['user'],
-          lastMessage: lastMessage['text'],
+          lastMessage: doc.data()['lastMessage'],
           avi: doc.data()['avi'],
         });
-        console.log(chats);
       });
 
       setChatList(chats);
     });
   };
+
+  const setLastMessageField = async (user, lastMessage) => {
+    const userRef = doc(db, 'users', 'Guest@buzz.com', 'chats', user);
+    await updateDoc(userRef, {lastMessage: lastMessage});
+  };
+
   useEffect(() => {
-    querySnapshot();
+    queryAllChats();
   }, []);
 
   return (
